94db3a15ce412ffa364ad6d3884de80385c79cf4
dlccyes <32875990+dlccyes@users.noreply.github.com>
2022-01-21 21:03:06 +0800

    preprocess alternate version

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,10 @@
 from pydub import AudioSegment
+# import subprocess
 
 def convert(path:str):
+  # file_name = path.rstrip('.mp4')
+  # command = f"ffmpeg -i {path} -ab 160k -ac 2 -ar 44100 -vn {file_name}.wav"
+  # subprocess.call(command, shell=True)
   print(path)
   AudioSegment.converter = r'C:\FFmpeg\bin\ffmpeg.exe'
   AudioSegment.ffprobe =r"C:\FFmpeg\bin\ffprobe.exe"
